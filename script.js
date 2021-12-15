f84a24172df7d10930aad842be81ba326ac768b1
Estebanadm <89546893+EstebanDuran8@users.noreply.github.com>
2021-12-15 17:33:54 -0600

    Finished about me page
    Debating on maintaining characters changing colors

--- script.js
+++ script.js
@@ -42,3 +42,17 @@ document.body.addEventListener('mousemove',(e) =>{
 //     mouseCircle.style.opacity='0';
 //     mouseDot.style.opacity='0';
 // })
+//About Me Text
+const aboutMeText=document.querySelector('.about-me-text')
+const aboutMeTextContent='My name is Esteban Alejandro Duran Marti,I am an international student from Bolivia that graduated from the university of Arkansas with a Bachelor of Science in Computer Science on fall 2021. I started my career as a candidate for a bachelor’s in electrical engineering but once I took my first programming class, I found my passion for coding. I focused on mobile development after my first Internship with J.B Hunt. This internship developed my passion for mobile development, and I worked with a full-time development team using react native. After a year with J.B Hunt I had the opportunity to make an internship with a startup company called ObiiGo. I worked as a Junior Developer and my role was to maintain the mobile application and determine the priority on bugs, new features and develop them. I look forward to being the best developer that I can be and keep growing professionally. ';
+console.info(Array.from(aboutMeTextContent));
+Array.from(aboutMeTextContent).forEach(char=>{
+    const span= document.createElement('span');
+    span.textContent=char;
+    aboutMeText.appendChild(span);
+
+    span.addEventListener('mouseenter',e=>{
+        e.target.style.animation="aboutMeTextAnim 10s infinite";
+    });
+});
+//End of about me text
